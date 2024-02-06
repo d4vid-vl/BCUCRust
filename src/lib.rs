f016316e8c05ed177b38e6b55e5ac201779579c6
pauzeql <david.verdugo.lizana@gmail.com>
2024-02-05 23:16:30 -0300

    added documentation aspects

---
+++ src/lib.rs
@@ -0,0 +1,12 @@
+//! BCUCRust (o BuscaCursosUCRust) es una librería de Rust para poder acceder a los cursos de la Universidad Católica de Chile con mayor facilidad.
+//! 
+//! # Instalación
+//!
+//! Añade lo siguiente a tu archivo `Cargo.toml`:
+//!
+//! ```toml
+//! [dependencies]
+//! BCUCRust = "0.1.0"
+//! ```
+
+pub mod cursos;
